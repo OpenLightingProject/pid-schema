e69bcc32c22b4b3a66488c9fc948072e49c99aef
Simon Newton <nomis52@gmail.com>
2015-09-28 20:11:13 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+  "use strict";
+  grunt.initConfig({
+    tv4: {
+      options: {
+        root: grunt.file.readJSON("schema.json"),
+        banUnknownProperties: true
+      },
+      myTarget: {
+        src: ["data/*.json"]
+      }
+    },
+    jshint: {
+      dev: [
+        "Gruntfile.js",
+        "schema.json",
+        "data/*.json"
+      ],
+      options: {
+        jshintrc: true
+      }
+    }
+  });
+  grunt.loadNpmTasks("grunt-tv4");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.registerTask("default", ["tv4"]);
+  grunt.registerTask("lint", ["jshint:dev"]);
+};
